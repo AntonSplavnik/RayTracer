1999707cf7cc9e1e7876a37ccaf4e8383575071d
LazyPixel <antonsplavnik@gmail.com>
2025-04-10 09:03:48 +0200

    strating point of the RayTracer with c++ and OpenGl

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#define GL_SILENCE_DEPRECATION
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
